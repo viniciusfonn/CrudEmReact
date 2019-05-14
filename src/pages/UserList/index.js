baeef96b33256b767936ce3cba1dd9b5e7a99cf6
vinicius <viniciusfon96@gmail.com>
2019-05-13 22:58:48 -0300

    criação do arquivo de conexão com a api e criação da rota de visualização de users

--- src/pages/UserList/index.js
+++ src/pages/UserList/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 export default function UserList(){
+    // State start as an empty array, setUsers can alternate users values
+    const [users, setUsers] = useState([]);
+
+    // the first parameter will be executed, the second is when the effect should be shotted,
+    // with an empty array, will be shotted only when the component be mounted
+    useEffect( ()=>{
+        async function loadUsers(){
+            const response = await api.get('/users');
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, []);
+
+    console.log(users);
+
     return (
-        <h1>Hello World</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</ th>
+                    <th>E-mail</ th>
+                    <th>Rua</ th>
+                    <th>Nº</ th>
+                    <th></ th>
+                </ tr>    
+            </ thead>
+            <tbody>
+                {users.map(user=>(
+                    <tr key={user._id}>
+                        <td> {user.name} </ td>
+                        <td> {user.email} </ td>
+                        <td> {user.address.street} </ td>
+                        <td> {user.address.number} </ td>
+                        <td> 
+                            <a href="" > Editar</ a>
+                         </ td>
+                    </ tr>
+                ))}
+            </ tbody>
+        </ table>
+
     );
 }
